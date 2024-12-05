e43c10eee7302f5abfc3c2f9f90a7465c5c59cf5
amosb <whirlymarbles@gmail.com>
2024-12-05 13:25:56 +0200

    shaddap

--- src/entity/Bomb.java
+++ src/entity/Bomb.java
@@ -3,6 +3,6 @@ package entity;
 public class Bomb extends Entity {
 	@Override
 	public String toString() {
-		return(String.format("Bomb object @("+x+", "+y+")"));
+		return (String.format("Bomb object @(" + x + ", " + y + ")"));
 	}
 }
